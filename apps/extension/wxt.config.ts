ac266c5b5ade02f2bdfaa9859bef580e65497511
YmlyZA <ksmac.edu@gmail.com>
2026-06-30 17:03:52 +0800

    feat(extension): add EchoFlow icon (soundwave over bilingual captions)

--- apps/extension/wxt.config.ts
+++ apps/extension/wxt.config.ts
@@ -9,9 +9,21 @@ export default defineConfig({
     version: "0.0.1",
     permissions: ["activeTab", "storage", "tabCapture", "offscreen", "scripting"],
     host_permissions: ["http://127.0.0.1/*", "http://localhost/*"],
+    icons: {
+      16: "/icon/16.png",
+      32: "/icon/32.png",
+      48: "/icon/48.png",
+      128: "/icon/128.png"
+    },
     action: {
       default_title: "EchoFlow",
-      default_popup: "popup.html"
+      default_popup: "popup.html",
+      default_icon: {
+        16: "/icon/16.png",
+        32: "/icon/32.png",
+        48: "/icon/48.png",
+        128: "/icon/128.png"
+      }
     }
   }
 });
